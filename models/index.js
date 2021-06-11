953762be19417ad24e6cad56029451eee4a1bbe0
forbegos <forbegos@me.com>
2021-06-11 17:15:51 -0600

    did some refactoring

--- models/index.js
+++ models/index.js
@@ -1,16 +1,6 @@
-const User = require("./User");
 const Lead = require("./Lead");
 const Task = require("./Task");
 const Client = require("./Client");
-
-// User.hasMany(Task, {
-//   foreignKey: "user_id",
-//   onDelete: "CASCADE",
-// });
-
-Task.belongsTo(User, {
-  foreignKey: "user_id",
-});
 
 Client.hasMany(Task, {
   foreignKey: "client_id",
@@ -21,31 +11,13 @@ Task.belongsTo(Client, {
   foreignKey: "client_id",
 });
 
-User.hasMany(Lead, {
-  foreignKey: "user_id",
-  onDelete: "CASCADE",
-});
-
 Client.hasMany(Lead, {
   foreignKey: "client_id",
   onDelete: "CASCADE",
-});
-
-Client.belongsTo(User, {
-  foreignKey: "user_id",
-  onDelete: "CASCADE",
-});
-
-User.hasMany(Client, {
-  foreignKey: "user_id",
-});
-
-Lead.belongsTo(User, {
-  foreignKey: "user_id",
 });
 
 Lead.belongsTo(Client, {
   foreignKey: "client_id",
 });
 
-module.exports = { User, Client, Task, Lead };
+module.exports = { Client, Task, Lead };
